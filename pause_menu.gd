69335a45f7d2154c1e13c7d6c61845392c7b76e7
sheep4mil-ui <301898834+sheep4mil-ui@users.noreply.github.com>
2026-07-30 23:40:04 -0500

    Add controller cursor clicks to menus

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,10 +1,20 @@
 extends CanvasLayer
 
+const MenuCursor = preload("res://inventory_cursor.gd")
+
 @onready var resume_button: Button = $Panel/VBoxContainer/ResumeButton
+@onready var quit_button: Button = $Panel/VBoxContainer/QuitButton
+
+var _cursor: Control
 
 
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
+	_cursor = MenuCursor.new()
+	_cursor.size = Vector2(22, 22)
+	_cursor.z_index = 100
+	_cursor.connect("primary_action", _activate_control_at_position)
+	add_child(_cursor)
 	hide()
 
 
@@ -21,11 +31,13 @@ func _input(event):
 func pause_game():
 	get_tree().paused = true
 	show()
-	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	_cursor.call("activate", get_viewport().get_visible_rect().size)
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	resume_button.grab_focus()
 
 
 func resume_game():
+	_cursor.call("deactivate")
 	get_tree().paused = false
 	hide()
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -37,3 +49,11 @@ func _on_resume_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _activate_control_at_position(global_cursor_position: Vector2) -> void:
+	for button in [resume_button, quit_button]:
+		if button.visible and not button.disabled and button.get_global_rect().has_point(global_cursor_position):
+			button.grab_focus()
+			button.pressed.emit()
+			return
